66442b98619780e438049d89efd00783fb07cd2d
josito19 <jose.iglesias@iesnervion.es>
2025-10-14 13:56:43 +0200

    Ejercicios ordinogramas 1-11

--- src/Diagrams/Ejercicio11.java
+++ src/Diagrams/Ejercicio11.java
@@ -1,10 +1,50 @@
 package Diagrams;
 
+import java.util.Scanner;
+
 public class Ejercicio11 {
 
 	public static void main(String[] args) {
+		// Creacion del escaner y las variables
+		Scanner sc = new Scanner(System.in);
 
-		
+		double horasTrabajadas;
+		double tarifaHoras;
+		double salarioBruto;
+		double salarioNeto;
+		double tasas;
+
+		System.out.println("Horas trabajadas:");
+		horasTrabajadas = sc.nextDouble();
+		System.out.println("Tarifa horas:");
+		tarifaHoras = sc.nextDouble();
+
+		if (horasTrabajadas < 35) {
+			salarioBruto = horasTrabajadas * tarifaHoras;
+		} else {
+			salarioBruto = 35 * tarifaHoras + (horasTrabajadas - 35) * tarifaHoras * 1.5;
+		}
+
+		if (salarioBruto <= 500) {
+			if (salarioBruto <= 900) {
+				tasas = salarioBruto * 0.45;
+				salarioNeto = salarioBruto - tasas;
+			} else {
+				tasas = salarioBruto * 0.25;
+				salarioNeto = salarioBruto - tasas;
+			}
+		} else {
+			tasas = salarioBruto * 0.25;
+			salarioNeto = salarioBruto - tasas;
+		}
+
+		// Mostramos el nombre, salarioBruto, tasas y salarioNeto
+		System.out.println("El empleado cobra:");
+		System.out.println("Salario Bruto: " + salarioBruto);
+		System.out.println("Salario Neto: " + salarioNeto);
+		System.out.println("Tasas: " + tasas);
+
+		sc.close();
 	}
 
 }
